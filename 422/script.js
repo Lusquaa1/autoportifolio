60d536a20a2e275208198f1c30a54fb03e76467e
Lucas Aguiar <43057423+Lusquaa1@users.noreply.github.com>
2020-08-29 17:32:42 -0300

    Today Added

--- 422/script.js
+++ 422/script.js
@@ -9,3 +9,44 @@ function navopen() {
 	l3.classList.toggle("l3open");
 	nv.classList.toggle("nvopen");
 }
+
+var data = new Date();
+var DiaS = data.getDay();
+var sem;
+
+const Seg = document.getElementById("Segunda");
+const Ter = document.getElementById("Terça");
+const Qua = document.getElementById("Quarta");
+const Qui = document.getElementById("Quinta");
+const Sex = document.getElementById("Sexta");
+
+var DiaSemana = document.getElementById("DiaSemana");
+
+switch (DiaS) {
+	case 1:
+		sem = "Segunda";
+		Seg.classList.toggle("today");
+		break;
+	case 2:
+		sem = "Terça";
+		Ter.classList.toggle("today");
+		break;
+	case 3:
+		sem = "Quarta";
+		Qua.classList.toggle("today");
+		break;
+	case 4:
+		sem = "Quinta";
+		Qui.classList.toggle("today");
+		break;
+	case 5:
+		sem = "Sexta";
+		Sex.classList.toggle("today");
+		break;
+	case 6:
+		sem = "Sábado";
+		break;
+	case 7:
+		sem = "Domingo";
+		break;
+}
